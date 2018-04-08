eeed540d99986dc18b7530f5b68302fe78c465c0
21354123 <yournokia@outlook.com>
2018-04-08 11:29:37 +0800

    添加项目文件。

---
+++ TestHttps/Controllers/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestHttps.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return Program.Values;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            if (id >= Program.Values.Count || id < 0) return "木有找到"; else return Program.Values[id];            
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+            Program.Values.Add(value);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void  Put(int id, [FromBody]string value)
+        {
+            if (id < Program.Values.Count && id >= 0) Program.Values[id] = value;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            if (id < Program.Values.Count && id >= 0) Program.Values.RemoveAt(id);
+        }
+    }
+}
---
+++ TestHttps/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System.Collections.Generic;
+using System.Net;
+namespace TestHttps
+{
+    public class Program
+    {
+        /// <summary>
+        /// 服务器端保存字符串的列表
+        /// </summary>
+        public static List<string> Values = new List<string>() { "value0", "value1" };
+        public static  IPAddress LocalIP;
+        public static void Main(string[] args)
+        {
+            string name = Dns.GetHostName();
+            LocalIP = Dns.GetHostAddresses(name)[0];
+            BuildWebHost(args).Run();
+
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+
+                .UseKestrel(options =>
+                {
+                    options.Listen(IPAddress.Any , 5001, listenOptions =>
+                    {
+                        listenOptions.UseHttps("d:\\iis.pfx", "yyz");
+                    });
+                    options.Listen(IPAddress.Any, 5002);
+                })
+
+                .Build();
+    }
+}
